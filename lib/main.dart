4c272b8a90e79ce4aece4679e947ea08ad050574
Mohammad-Ihmeid <mohammadmaher1999@yahoo.com>
2024-07-07 13:01:44 +0300

    Add Firebase To Progect

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:montra_app/core/extensions/context_extension.dart';
 import 'package:montra_app/core/res/theme_data/theme_data_light.dart';
 import 'package:montra_app/core/services/injection_container.dart';
 import 'package:montra_app/core/services/router.dart';
+import 'package:montra_app/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await init();
   runApp(const MyApp());
 }
